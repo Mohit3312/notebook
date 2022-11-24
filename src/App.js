fb68719cab38892fb31990241e41147abc9f6296
Mohit3312 <mohit3312@gmail.com>
2022-11-25 00:03:37 +0530

    Created Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Notebook app</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>This is Notebook app</div>;
+};
+
+export default Home;
