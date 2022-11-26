6ad4d91cd4c3d7e9b5b0639539504b58c9171d57
Mohit3312 <mohit3312@gmail.com>
2022-11-26 15:21:41 +0530

    Added a Modal for Editing Notes

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,7 +4,7 @@ import NoteContext from "../context/notes/NoteContext";
 const NoteItem = (props) => {
   const context = useContext(NoteContext);
   const { deleteNote } = context;
-  const { note } = props;
+  const { note, editNote } = props;
   return (
     <div className="col-md-3">
       <div className="card my-3">
@@ -17,7 +17,12 @@ const NoteItem = (props) => {
                 deleteNote(note._id);
               }}
             ></i>
-            <i className="fa-solid fa-pen-to-square mx-2"></i>
+            <i
+              className="fa-solid fa-pen-to-square mx-2"
+              onClick={() => {
+                editNote(note);
+              }}
+            ></i>
           </div>
           <p className="card-text">{note.description}</p>
         </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -19,7 +19,6 @@ const NoteState = (props) => {
       },
     });
     const json = await response.json();
-    console.log(json);
     setNotes(json);
   };
 
@@ -36,21 +35,35 @@ const NoteState = (props) => {
       body: JSON.stringify({ title, description, tag }),
     });
 
+    const json = await response.json();
+    console.log(json);
+
     const note = {
-      _id: "637f65f4fd8e00d382d01db2",
-      user: "637e5d6159dcdb813b8cd6be",
+      _id: json._id,
+      user: json.user,
       title: title,
       description: description,
       tag: tag,
-      date: "2022-11-24T12:39:16.205Z",
-      __v: 0,
+      date: json.date,
+      __v: json.__v,
     };
     setNotes(notes.concat(note));
   };
 
   // Delete a Note
-  const deleteNote = (id) => {
+  const deleteNote = async (id) => {
     // TODO: API Call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM3ZTVkNjE1OWRjZGI4MTNiOGNkNmJlIn0sImlhdCI6MTY2OTIyODk3NH0.ye34a_CUI9xElFvJ4H0-atwBZ0pf0wN_Rv4RO1rA3RQ",
+      },
+    });
+    const json = await response.json();
+    console.log(json);
+
     const newNotes = notes.filter((note) => {
       return note._id !== id;
     });
@@ -60,7 +73,7 @@ const NoteState = (props) => {
   // Edit a Note
   const editNote = async (id, title, description, tag) => {
     // TODO: API Call
-    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+    await fetch(`${host}/api/notes/updatenote/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -69,7 +82,6 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({ title, description, tag }),
     });
-    const json = response.json();
     // Logic to edit on client side
     for (let index = 0; index < notes.length; index++) {
       const element = notes[index];
